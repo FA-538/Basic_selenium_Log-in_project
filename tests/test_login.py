894a561a0c48508f35ab3aac992f7f1474f9ee7a
Faysal Ahmed <faysalahmed.fa538@gmail.com>
2026-04-12 19:12:34 +0200

    Migrated login test to pytest using fixture setup

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,10 +7,10 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def test_valid_login(driver):
     wait = WebDriverWait(driver, 10)
-    driver.get("https://opensource-demo.com/")
-    self.wait.until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
-    self.driver.find_element(By.NAME, "password").send_keys("admin123")
-    self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-    self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "oxd-userdropdown-tab"))).click()
-    self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "ul.oxd-dropdown-menu[role='menu']")))
-    self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Logout']"))).click()
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    wait.until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
+    driver.find_element(By.NAME, "password").send_keys("admin123")
+    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "oxd-userdropdown-tab"))).click()
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "ul.oxd-dropdown-menu[role='menu']")))
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Logout']"))).click()
