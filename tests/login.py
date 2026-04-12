f63458683ecff9c1249d875f777c6da5d5d0cde2
Faysal Ahmed <faysalahmed.fa538@gmail.com>
2026-04-12 16:17:00 +0200

    Modified login test using Unittest structure with setup and Teardown

--- tests/login.py
+++ tests/login.py
@@ -2,23 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import unittest
 
 
-#Simple login and logout test using Selenium
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get("https://opensource-demo.orangehrmlive.com/")
+class LoginTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.maximize_window()
+        cls.driver.get("https://opensource-demo.orangehrmlive.com/")
+        cls.wait = WebDriverWait(cls.driver, 10)
 
-wait = WebDriverWait(driver, 10)
 
-#log-in
-wait.until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
-driver.find_element(By.NAME, "password").send_keys("admin123")
-driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    def test_valid_login(self):
+        self.wait.until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+        self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "oxd-userdropdown-tab"))).click()
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "ul.oxd-dropdown-menu[role='menu']")))
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Logout']"))).click()
 
-#Log-Out
-wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "oxd-userdropdown-tab"))).click()
-wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "ul.oxd-dropdown-menu[role='menu']")))
-wait.until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Logout']"))).click()
-driver.quit()
-print("Test Completed")
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        print("Test Completed")
+
+if __name__ == '__main__':
+    unittest.main()
